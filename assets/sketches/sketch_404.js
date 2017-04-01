d97b554f80a6579a06f5fd75ec78d7fa9ab7696b
milkyklim <klim.kolyvanov@gmail.com>
2017-04-01 22:31:08 +0200

    changed the sketch

--- assets/sketches/sketch_404.js
+++ assets/sketches/sketch_404.js
@@ -1,16 +1,42 @@
-  function setup() {
-    myCanvas = createCanvas(720, 400);
-    myCanvas.parent('404_sketch');
-  }
+var message = "404",
+  font,
+  bounds, // holds x, y, w, h of the text's bounding box
+  fontsize = 60,
+  x, y; // x and y coordinates of the text
 
-  function draw() {
-    background(127);
-    noStroke();
-    for (var i = 0; i < height; i += 20) {
-      fill(129, 206, 15);
-      rect(0, i, width, 10);
-      fill(255);
-      rect(i, 0, 10, height);
-    }
-    noLoop();
+function preload() {
+  font = loadFont('/assets/fonts/FontAwesome.otf');
+}
+
+function setup() {
+  myCanvas = createCanvas(720, 400);
+  myCanvas.parent('sketch_404');
+
+  // set up the font
+  textFont('FontAwesome');
+  textSize(fontsize);
+
+  // get the width and height of the text so we can center it initially
+  bounds = font.textBounds(message, 0, 0, fontsize);
+  x = width / 2 - bounds.w / 2;
+  y = height / 2 - bounds.h / 2;
+}
+
+function draw() {
+  // instead of clearing the background, fade it by drawing
+  // a semi-transparent rectangle on top
+  fill(255, 120);
+  rect(-3, -3, width + 3, height + 3);
+
+  // write the text in black and get its bounding box
+  fill(0);
+  text(message, x, y);
+  bounds = font.textBounds(message,x,y,fontsize);
+
+  // check if the mouse is inside the bounding box and tickle if so
+  if ( mouseX >= bounds.x && mouseX <= bounds.x + bounds.w &&
+    mouseY >= bounds.y && mouseY <= bounds.y + bounds.h) {
+    x += random(-5, 5);
+    y += random(-5, 5);
   }
+}
